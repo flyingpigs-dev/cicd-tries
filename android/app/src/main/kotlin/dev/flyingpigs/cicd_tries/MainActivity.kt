53506b1b4c229b0f7b09a5f5f066da697ab1d00c
Ekrem Moralioglu <ekoremdev@gmail.com>
2023-11-02 23:38:04 +0400

    cicd

--- android/app/src/main/kotlin/dev/flyingpigs/cicd_tries/MainActivity.kt
+++ android/app/src/main/kotlin/dev/flyingpigs/cicd_tries/MainActivity.kt
@@ -2,5 +2,9 @@ package dev.flyingpigs.cicd_tries
 
 import io.flutter.embedding.android.FlutterActivity
 
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
+
 class MainActivity: FlutterActivity() {
 }
